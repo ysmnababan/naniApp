3f5d73d1a51a0360a4de331e30c3ca0f1f2d8d19
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-10-05 00:47:42 +0700

    feat: add implementation of 'user service' usecase

--- app/user_service/usecase/user_usecase.go
+++ app/user_service/usecase/user_usecase.go
@@ -4,15 +4,17 @@ import (
 	"fmt"
 	"time"
 	"user_service/domain"
-	"user_service/infrastructure/database/models"
+	"user_service/helper"
 	"user_service/infrastructure/repository"
 
 	"github.com/golang-jwt/jwt"
+	"github.com/samborkent/uuidv7"
 	"golang.org/x/crypto/bcrypt"
 )
 
 var TOKEN_KEY = "THIS IS TOKEN KEY"
-func generateToken(u *models.User) (string, error) {
+
+func generateToken(u *domain.User) (string, error) {
 	// create the payload
 	payload := jwt.MapClaims{
 		"id":    u.UserID,
@@ -37,24 +39,81 @@ type UserUsecase struct {
 }
 
 type UserUsecaseI interface {
-	Login(user *domain.User) (string, error)
+	Login(email, password string) (string, error)
+	Register(user *domain.User) error
+	GetUserData(user_id string) (*domain.User, error)
+	UpdateUserData(user *domain.User) error
 }
 
-
-func (u *UserUsecase) Login(user *domain.User) (string, error) {
-	userModel, err := u.UserRepositoryI.FetchUserByEmail(user.Email)
+func (u *UserUsecase) Login(email, password string) (string, error) {
+	userDomain, err := u.UserRepositoryI.FetchUserByEmail(email)
 	if err != nil {
 		return "", err
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(userModel.Password), []byte(user.Password))
+	err = bcrypt.CompareHashAndPassword([]byte(userDomain.Password), []byte(password))
 	if err != nil {
 		return "", err
 	}
 
-	tokenString, err := generateToken(userModel)
+	tokenString, err := generateToken(userDomain)
 	if err != nil {
 		return "", nil
 	}
 	return tokenString, nil
+}
+
+func (u *UserUsecase) Register(user *domain.User) error {
+	err := u.UserRepositoryI.IsUserExist(user.Email, user.PhoneNumber)
+	if err != nil {
+		return err
+	}
+
+	// generate uuid
+	uuidV7 := uuidv7.New()
+	user.UserID = uuidV7.String()
+
+	// hash password
+	hashedpwd, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return helper.ErrParam
+	}
+	user.Password = string(hashedpwd)
+
+	err = u.UserRepositoryI.CreateUser(user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *UserUsecase) GetUserData(user_id string) (*domain.User, error) {
+	user, err := u.UserRepositoryI.FetchUserByID(user_id)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *UserUsecase) UpdateUserData(user *domain.User) error {
+	// fetch data from databasse first,
+	// if req data is not empty, then update
+	userDomain, err := u.UserRepositoryI.FetchUserByID(user.UserID)
+	if err != nil {
+		return err
+	}
+
+	// username cannot be empty
+	if user.Username != "" {
+		userDomain.Username = user.Username
+	}
+	userDomain.Picture_URL = user.Picture_URL
+
+	// update user data
+	err = u.UserRepositoryI.UpdateUser(user)
+	if err != nil {
+		return err
+	}
+	return nil
 }
