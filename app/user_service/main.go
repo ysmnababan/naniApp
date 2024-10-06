7bdb87653f84ae6957344515578ba9a91c2b0911
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2024-10-06 22:26:13 +0700

    feat: start grpc server

--- app/user_service/main.go
+++ app/user_service/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"log"
+	"net"
 	"user_service/infrastructure/database"
+	"user_service/infrastructure/database/models"
 	"user_service/infrastructure/repository"
+	grpchandler "user_service/interface/grpc_handler"
+	"user_service/interface/grpc_handler/pb"
+	"user_service/usecase"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
 	db := database.Connect()
+	db.AutoMigrate(models.User{})
+	repo := &repository.Repo{DB: db}
+	userUsecase := &usecase.UserUsecase{UserRepositoryI: repo}
+	userHandler := &grpchandler.UserHandler{UserUsecaseI: userUsecase}
 
-	Repo := &repository.Repo{DB: db}
-	
-	Repo.DB.AutoMigrate()
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, userHandler)
+	listen, err := net.Listen("tcp", ":50001")
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = grpcServer.Serve(listen)
+	if err != nil {
+		log.Println(err)
+	}
 }
